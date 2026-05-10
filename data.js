ef29fbd96a00bc20bc4aca60d692627abb2e55e8
mittisomarket <as.suxiy@gmail.com>
2026-05-10 08:32:21 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+export const initialCustomers = [
+  { id: 'CUST-001', name: 'Aziz Karimov', phone: '+998901234567', address: 'Tashkent, Yunusabad' },
+  { id: 'CUST-002', name: 'Dilnoza Aliyeva', phone: '+998931112233', address: 'Samarkand, Center' },
+  { id: 'CUST-003', name: 'Rustam Qosimov', phone: '+998975556677', address: 'Bukhara, Old City' }
+];
+
+export const initialProducts = [
+  { 
+    id: 'PROD-001', 
+    name: 'iPhone 15 Pro Max', 
+    price: 15000000, 
+    qoldiq: 10, 
+    tavsif: '256GB, Natural Titanium. Eng yangi flagman telefon.', 
+    image: 'https://images.unsplash.com/photo-1696446701796-da61225697cc?w=400' 
+  },
+  { 
+    id: 'PROD-002', 
+    name: 'MacBook Air M2', 
+    price: 12000000, 
+    qoldiq: 3, 
+    tavsif: '8/256GB, Space Gray. Yengil va kuchli noutbuk.', 
+    image: 'https://images.unsplash.com/photo-1611186871348-b1ec696e523b?w=400' 
+  },
+  { 
+    id: 'PROD-003', 
+    name: 'Samsung Galaxy S23 Ultra', 
+    price: 13000000, 
+    qoldiq: 7, 
+    tavsif: '512GB, Green. Zo\'r kamera va displey.', 
+    image: 'https://images.unsplash.com/photo-1678911820864-e2c567c655d7?w=400' 
+  },
+  { 
+    id: 'PROD-004', 
+    name: 'AirPods Pro 2', 
+    price: 3000000, 
+    qoldiq: 15, 
+    tavsif: 'Shovqinni bosuvchi simsiz quloqchinlar.', 
+    image: 'https://images.unsplash.com/photo-1588423770574-9102111d4194?w=400' 
+  }
+];
+
+export const initialSales = [];
+
+export const initialNews = [
+  { 
+    id: 'NEWS-001', 
+    title: 'Uzum Market uslubiga o\'tdik!', 
+    content: 'Bizning ilovamiz endi yanada qulay va chiroyli dizaynga ega bo\'ldi. Yangiliklarni kuzatib boring.', 
+    date: '2026-05-10',
+    image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=800'
+  }
+];
